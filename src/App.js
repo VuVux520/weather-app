9d8d2de9aa8b876335a7f5b8e24902a1b0be93e0
VuVux99 <vycaoqer@gmail.com>
2021-07-13 09:55:40 +0700

    update position

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import ReactWeather, { useOpenWeather } from 'react-open-weather';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
-  const { data, isLoading, errorMessage } = useOpenWeather({
-    key: 'f8380a9a12337a0ccff953df033ba08f',
-    lat: '16.047079',
-    lon: '108.206230',
-    lang: 'en',
-    unit: 'metric',
-  });
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  // const { data, errorMessage } = useOpenWeather({
+  //   key: 'f8380a9a12337a0ccff953df033ba08f',
+  //   lat: lat,
+  //   lon: long,
+  //   lang: 'en',
+  //   unit: 'metric',
+  // });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+    fetchData();
+  }, [lat,long])
+
   return (
     <div className="App">
-      <ReactWeather
-        isLoading={isLoading}
-        errorMessage={errorMessage}
-        data={data}
-        lang="en"
-        locationLabel="Đà Nẵng"
-        unitsLabels={{ temperature: 'C', windSpeed: 'Km/h' }}
-        showForecast
-      />
+        <ReactWeather
+          isLoading={isLoading}
+          data={data}
+          lang="en"
+          unitsLabels={{ temperature: 'C', windSpeed: 'Km/h' }}
+          showForecast
+        />
     </div>
   );
 }
