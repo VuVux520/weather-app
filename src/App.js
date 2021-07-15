00476c22e8aeffcb82850dbb693cb2892b34cd7a
VuVux520 <vycaoqer@gmail.com>
2021-07-15 11:00:03 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,28 +5,18 @@ import React, {useState, useEffect} from 'react';
 function App() {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  // const { data, errorMessage } = useOpenWeather({
-  //   key: 'f8380a9a12337a0ccff953df033ba08f',
-  //   lat: lat,
-  //   lon: long,
-  //   lang: 'en',
-  //   unit: 'metric',
-  // });
+  const { data, isLoading, errorMessage } = useOpenWeather({
+    lat: 16.047079,
+    lon: 108.206230,
+    lang: 'en',
+    unit: 'metric',
+  });
 
   useEffect(() => {
     const fetchData = async () => {
       navigator.geolocation.getCurrentPosition(function(position) {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
-      });
-
-      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-      .then(res => res.json())
-      .then(result => {
-        setData(result)
-        console.log(result);
       });
     }
     fetchData();
@@ -36,6 +26,7 @@ function App() {
     <div className="App">
         <ReactWeather
           isLoading={isLoading}
+          errorMessage={errorMessage}
           data={data}
           lang="en"
           unitsLabels={{ temperature: 'C', windSpeed: 'Km/h' }}
